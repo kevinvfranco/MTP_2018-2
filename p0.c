6ed54cbd83fa3b5cf5fb939aa72da8fd24cfc93c
kevinvfranco <42453809+kevinvfranco@users.noreply.github.com>
2018-09-03 20:36:31 -0300

    Update p0.c

--- p0.c
+++ p0.c
@@ -2,16 +2,17 @@
 /* Kevin Vinicius Franco */
 /* 11811EEL007 */
 #include <stdio.h>
+
 int main(int argc, char ** argv) {
 int i;
 if(argc > 1) {
-if(argc > 2)
-for(i = 0; i < argv[2][0]-'0'; i++)
-printf("Valeu, %s\n", argv[1]);
+    if(argc > 2)
+        for(i = 0; i < argv[2][0]-'0'; i++)
+            printf("Valeu, %s\n", argv[1]);
+    else
+        printf("Saudacoes, %s\n", argv[1]);
+ }
 else
-printf("Saudacoes, %s\n", argv[1]);
-}
-else
-printf("Ola!\n");
+    printf("Ola!\n");
 return 0;
 }
