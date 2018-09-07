4bdb11b39ff087ceddc551c22e801dbec00e7f2f
kevinvfranco <42453809+kevinvfranco@users.noreply.github.com>
2018-09-07 17:31:04 -0300

    Update p1.c

--- p1.c
+++ p1.c
@@ -4,22 +4,18 @@
 #include <stdio.h>
 
 int main(){
-    int estado = 0, i, estado_if;
+    int estado = 0, i;
     char bits[256];
     printf("Digite a sequencia binaria: ");
     scanf("%s", bits);
     for(i = 0; bits[i] != '\0'; i++){
         if(estado == 0)
-            estado_if = bits[i] == '0' ? 0 : 1;
+            estado = bits[i] == '0' ? 0 : 1;
         else if(estado == 1)
-            estado_if = bits[i] == '0' ? 2 : 0;
+            estado = bits[i] == '0' ? 2 : 0;
         else
-            estado_if = bits[i] == '0' ? 1 : 2;
-        estado = estado_if;
+            estado = bits[i] == '0' ? 1 : 2;
     }
-    if(estado == 0)
-        printf("%s eh multiplo de 3",bits);
-    else
-        printf("%s nao eh multiplo de 3",bits);
+    printf("%s %s", bits, estado == 0 ? "eh multiplo de 3" : "nao eh multiplo de 3");
     return 0;
 }
